e5a27ae91bffa5c2de0daa02dd548ef4fe79944e
Sara Grijalba <grijalbapsaras.cla@gmail.com>
2026-04-04 17:43:50 -0500

    feat: ..

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 
 import { UserModule } from './user/user.module';
 import { RoleModule } from './role/role.module';
+import { RoleModule } from './role/role.module';
 
 @Module({
     providers: [],
--- src/auth/role/role.module.ts
+++ src/auth/role/role.module.ts
@@ -1,15 +1,4 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
 
-import { Role } from '../entities/role.entity';
-
-import { RoleService } from './role.service';
-import { RoleController } from './role.controller';
-
-@Module({
-    providers: [RoleService],
-    imports: [TypeOrmModule.forFeature([Role])],
-    exports: [RoleService],
-    controllers: [RoleController],
-})
+@Module({})
 export class RoleModule {}
