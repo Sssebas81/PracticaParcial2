e138cace128e0be9f8103bd1b93a5fda5928e1f8
Sara Grijalba <grijalbapsaras.cla@gmail.com>
2026-04-06 20:33:50 -0500

    ...

--- src/games/game/game.module.ts
+++ src/games/game/game.module.ts
@@ -5,8 +5,8 @@ import { UserModule } from '@/auth/user/user.module';
 
 import { Game } from '../entities/game.entity';
 
-import { GameService } from './game.service';
 import { GameController } from './game.controller';
+import { GameService } from './game.service';
 
 @Module({
     controllers: [GameController],
